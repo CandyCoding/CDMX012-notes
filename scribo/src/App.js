063afccb081c5a6b4b726fd99f0abb933091749f
Candy Itzel Lopez Enriquez <candylopez@iMac-de-Candy.local>
2022-04-22 19:56:56 -0500

    Instalación y configuración de Esl int y Prettier

--- scribo/src/App.js
+++ scribo/src/App.js
@@ -1,26 +1,26 @@
-import {BrowserRouter,Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import HomePage from './pages/HomePage';
-import RegisterPage from './pages/RegisterPage';
-import NotesPage from './pages/NotesPage';
-import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage'
+import RegisterPage from './pages/RegisterPage'
+import NotesPage from './pages/NotesPage'
+import NotFoundPage from './pages/NotFoundPage'
 
-import NavBar from './Components/NavBar';
-import './App.css';
+import NavBar from './Components/NavBar'
+import './App.css'
 
-function App() { 
+function App () {
   return (
     <BrowserRouter className="App">
       <NavBar />
-      
+
       <Routes>
-      <Route path="/" element={<HomePage/>}/> 
-      <Route path="/registro" element={<RegisterPage/>}/> 
-      <Route path="/notas" element={<NotesPage/>}/> 
-      <Route path="*" element={<NotFoundPage/>}/> 
-      </Routes> 
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/registro" element={<RegisterPage/>}/>
+      <Route path="/notas" element={<NotesPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
